b681705f75f8a87810e6c55f59d0a381bf6e763c
ThatBlokeJosh <joshtomrem@protonmail.com>
2024-12-11 14:13:25 +0100

    Day 11 updates

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -4,35 +4,22 @@ use std::{u64, vec};
 
 pub fn part1() -> std::io::Result<()> {
     let line = fs::read_to_string("./src/inputs/11.txt").unwrap();
-    let mut numbers: Vec<u64> = line.trim().split(" ").map(|n| n.parse::<u64>().unwrap()).collect();
+    let numbers: Vec<u64> = line.trim().split(" ").map(|n| n.parse::<u64>().unwrap()).collect();
+    let mut cache: HashMap<(u64, u8), u64> = HashMap::new();
 
-    for _i in 0..25 {
-        let mut new_nums: Vec<u64> = vec![]; 
-        for n in numbers {
-            match n {
-               0 => {new_nums.push(1);} 
-               _ => {
-                    let digits = n.ilog10() + 1;
-                    if digits % 2 == 0 {
-                        let half = (10 as u64).pow(digits / 2);
-                        new_nums.push(n / half);
-                        new_nums.push(n % half);
-                    } else {
-                        new_nums.push(n*2024);
-                    }
-               }
-            }
-        }
-        numbers = new_nums;
+    let mut counter = 0;
+
+    for n in numbers {
+        counter += blink(n, 25, &mut cache);
     }
 
-    println!("{:?}", numbers.len());
+    println!("{:?}", counter);
 
     return Ok(());
 }
 
-fn blink(number: u64, depth: u64, cache: &mut HashMap<(u64, u64), u64>) -> u64 {
-    if depth == 75 {
+fn blink(number: u64, depth: u8, cache: &mut HashMap<(u64, u8), u64>) -> u64 {
+    if depth == 0 {
         return 1;
     }
 
@@ -42,7 +29,7 @@ fn blink(number: u64, depth: u64, cache: &mut HashMap<(u64, u64), u64>) -> u64 {
 
     match number {
        0 => {
-            let res = blink(1, depth + 1, cache);
+            let res = blink(1, depth - 1, cache);
             cache.insert((number, depth), res);
             return res;
        } 
@@ -50,11 +37,11 @@ fn blink(number: u64, depth: u64, cache: &mut HashMap<(u64, u64), u64>) -> u64 {
             let digits = number.ilog10() + 1;
             if digits % 2 == 0 {
                 let half = (10 as u64).pow(digits / 2);
-                let res = blink(number / half, depth + 1, cache) + blink(number % half, depth + 1, cache);
+                let res = blink(number / half, depth - 1, cache) + blink(number % half, depth - 1, cache);
                 cache.insert((number, depth), res);
                 return res;
             } else {
-                let res = blink(number*2024, depth + 1, cache);
+                let res = blink(number*2024, depth - 1, cache);
                 cache.insert((number, depth), res);
                 return res;
             }
@@ -67,10 +54,10 @@ pub fn part2() -> std::io::Result<()> {
     let numbers: Vec<u64> = line.trim().split(" ").map(|n| n.parse::<u64>().unwrap()).collect();
 
     let mut counter = 0;
-    let mut cache: HashMap<(u64, u64), u64> = HashMap::new();
+    let mut cache: HashMap<(u64, u8), u64> = HashMap::new();
 
     for n in numbers {
-        counter += blink(n, 0, &mut cache);
+        counter += blink(n, 75, &mut cache);
     }
     println!("{:?}", counter);
 
